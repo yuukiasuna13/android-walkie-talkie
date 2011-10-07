fc6c9ac78e58d3f93d8377ce4b76414aebf948b2
kulkarni.pradnya <kulkarni.pradnya@gmail.com>
2011-10-07 02:59:09 +0000

    Main java file

---
+++ walkie-talkie/src/com/csc775/WalkieTalkie.java
@@ -0,0 +1,13 @@
+package com.csc775;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WalkieTalkie extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
